757bb63f408384d6c9290ae1b05f4fc5b9accdf5
Lili Boxer <ninaliboxer@gmail.com>
2019-07-17 09:13:58 -0700

    person schema set up

---
+++ mongoose-rels/lib/models/Person.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  city: String,
+  profileImage: String,
+  email: String
+});
+
+module.exports = mongoose.model('Person', personSchema);
